c0a9e8404634ecee4e3aa75c59cc5921ac6c8832
NMHieu9779 <nmhieu9779@hotmail.com>
2020-07-16 23:19:18 +0700

    add HomePage

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
+/**
+ *
+ * HomePage
+ *
+ */
 
-export function HomePage() {
+import React, { memo } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+import { useSelector, useDispatch } from 'react-redux';
+import styled from 'styled-components/macro';
+
+import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
+import { reducer, sliceKey } from './slice';
+import { selectHomePage } from './selectors';
+import { homePageSaga } from './saga';
+
+interface Props {}
+
+export const HomePage = memo((props: Props) => {
+  useInjectReducer({ key: sliceKey, reducer: reducer });
+  useInjectSaga({ key: sliceKey, saga: homePageSaga });
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const homePage = useSelector(selectHomePage);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const dispatch = useDispatch();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { t, i18n } = useTranslation();
+
   return (
     <>
       <Helmet>
-        <title>Home Page</title>
-        <meta name="description" content="A Boilerplate application homepage" />
+        <title>HomePage</title>
+        <meta name="description" content="Description of HomePage" />
       </Helmet>
-      <span>HomePage container</span>
+      <Div>{t('')}</Div>
     </>
   );
-}
+});
+
+const Div = styled.div``;
